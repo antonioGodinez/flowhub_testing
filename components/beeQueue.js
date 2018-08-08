1e98934b62b79072751be1878615543832e4d68f
Antonio Godinez <delacruzgodinezfernandoantonio@gmail.com>
2018-08-08 16:13:49 -0700

    Adde bee-queue component

---
+++ components/beeQueue.js
@@ -0,0 +1,40 @@
+const noflo = require('noflo');
+const Queue = require('bee-queue');
+
+exports.getComponent = () => {
+  const c = new noflo.Component();
+
+  c.description = 'Creates a beeQueue';
+
+  c.inPorts.add('queueName', {
+    datatype: 'string',
+    require: true,
+  });
+  c.inPorts.add('redisHost', {
+    datatype: 'string',
+    require: true,
+  });
+
+  c.outPorts.add('queue', {
+    datatype: 'any',
+  });
+
+  c.process((input, output) => {
+    if(!input.hasData('queueName', 'redisHost')) return;
+    const queueName = input.getData(queueName);
+    const redisHost = input.getData(queueName);
+
+    const queue = new Queue(queueName, {
+      redis: {
+        url: redisHost,
+      },
+      isWorker: true,
+      removeOnSuccess: false,
+    });
+
+    output.done();
+  });
+
+
+  return c;
+};
